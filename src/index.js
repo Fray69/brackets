bccc2e8640de07eb608e8b4ac873f1db6dc4acca
Fray69 <yura.rogovsky2001@gmail.com>
2021-02-27 15:22:25 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let open = [];
+    let close = [];
+    let brack = [];
+    let pairsofbrack = [];
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
+            pairsofbrack.push(bracketsConfig[i][0]);
+        } else {
+            open.push(bracketsConfig[i][0]);
+            close.push(bracketsConfig[i][1]);
+        }
+    }
+    
+    console.log(open);
+      
+    str = str.split('');
+    for (let i = 0; i < str.length; i++) {
+        console.log(brack);
+        if (open.includes(str[i])) {
+            brack.push(str[i]);  
+            
+        } else if (close.includes(str[i])) {
+            if (brack[brack.length-1] == open[close.indexOf(str[i])]) {
+                brack.pop();     
+            } else {
+                return false;
+            }
+        } else if (pairsofbrack.includes(str[i])) {
+            if (brack[brack.length-1] == str[i]){
+                brack.pop();
+            } else if (!brack.includes(str[i])) {
+                brack.push(str[i]);
+            } else {
+                return false;
+            }
+            
+        } 
+    }
+    
+    if (brack.length != 0) {
+        return false;
+    }
+    return true;
 }
